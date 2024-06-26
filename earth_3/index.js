030d9c4c027473e20c2ef0baef3362116ff0be6e
BigIskander <bigiskander@gmail.com>
2024-06-26 13:47:59 +0300

    Earth tilt and rotation.

--- earth_3/index.js
+++ earth_3/index.js
@@ -1,6 +1,5 @@
 import * as THREE from "three";
 import { OrbitControls } from "jsm/controls/OrbitControls.js";
-
 const w = window.innerWidth;
 const h = window.innerHeight;
 const scene = new THREE.Scene();
@@ -10,6 +9,9 @@ const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(w, h);
 document.body.appendChild(renderer.domElement);
 
+const earthGroup = new THREE.Group();
+earthGroup.rotation.z = -23.4 * Math.PI / 180;
+scene.add(earthGroup);
 new OrbitControls(camera, renderer.domElement);
 const loader = new THREE.TextureLoader();
 const geometry = new THREE.IcosahedronGeometry(1, 12);
@@ -17,14 +19,13 @@ const material = new THREE.MeshStandardMaterial({
     map: new THREE.TextureLoader().load("./textures/00_earthmap1k.jpg")
 });
 const earthMesh = new THREE.Mesh(geometry, material);
-scene.add(earthMesh);
+earthGroup.add(earthMesh);
 
 const hemiLight = new THREE.HemisphereLight(0xffffff, 0x000000);
 scene.add(hemiLight);
 function animate() {
     requestAnimationFrame(animate);
 
-	earthMesh.rotation.x += 0.001;
 	earthMesh.rotation.y += 0.002;
 	renderer.render( scene, camera );
 }
