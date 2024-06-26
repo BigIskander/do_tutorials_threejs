19521c090cde498181bb0efbdbc1ad36c619713d
BigIskander <bigiskander@gmail.com>
2024-06-26 11:41:42 +0300

    Just load the latest version.

--- getting_started/index.js
+++ getting_started/index.js
@@ -1,1 +1,8 @@
-console.log('Robot Bobby was here');
+import * as THREE from "three";
+
+const w = window.innerWidth;
+const h = window.innerHeight;
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+
+renderer.setSize(w, h);
+document.body.appendChild(renderer.domElement);
