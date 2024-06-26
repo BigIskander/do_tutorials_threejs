678d3440f92deedfdb5f19044265b5a0d743a00a
BigIskander <bigiskander@gmail.com>
2024-06-26 17:49:04 +0300

    Flythrough_framewire done.

--- flytru_wireframe/index.js
+++ flytru_wireframe/index.js
@@ -1,10 +1,14 @@
 import * as THREE from "three";
 import { OrbitControls } from "jsm/controls/OrbitControls.js";
 import spline from "./spline.js";
+import { EffectComposer } from "jsm/postprocessing/EffectComposer.js";
+import { RenderPass } from "jsm/postprocessing/RenderPass.js";
+import { UnrealBloomPass } from "jsm/postprocessing/UnrealBloomPass.js";
 
 const w = window.innerWidth;
 const h = window.innerHeight;
 const scene = new THREE.Scene();
+scene.fog = new THREE.FogExp2(0x000000, 0.3);
 const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
 camera.position.z = 5;
 const renderer = new THREE.WebGLRenderer();
@@ -15,6 +19,16 @@ const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 controls.dampingFactor = 0.03;
 
+// post-processing
+const renderScene = new RenderPass(scene, camera);
+const bloomPass = new UnrealBloomPass(new THREE.Vector2(w, h), 1.5, 0.4, 100);
+bloomPass.threshold = 0.002;
+bloomPass.strength = 3.5;
+bloomPass.radius = 0;
+const composer = new EffectComposer(renderer);
+composer.addPass(renderScene);
+composer.addPass(bloomPass);
+
 // create a line geometry from the spline
 const points = spline.getPoints(100);
 const geometry = new THREE.BufferGeometry().setFromPoints(points);
@@ -24,20 +38,65 @@ const line = new THREE.Line(geometry, material);
 
 // create a tube geometry from the spline
 const tubeGeo = new THREE.TubeGeometry(spline, 222, 0.65, 16, true);
-const tubeMat = new THREE.MeshStandardMaterial({
-    color: 0x0099ff,
-    side: THREE.DoubleSide,
-    wireframe: true,
-});
-const tube = new THREE.Mesh(tubeGeo, tubeMat);
-scene.add(tube);
+// const tubeMat = new THREE.MeshBasicMaterial({
+//     color: 0xffffff,
+//     // side: THREE.DoubleSide,
+//     wireframe: true,
+// });
+// const tube = new THREE.Mesh(tubeGeo, tubeMat);
+// scene.add(tube);
 
-const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444);
-scene.add(hemiLight);
-function animate() {
+// create edges geometry from the spline
+const edges = new THREE.EdgesGeometry(tubeGeo, 0.2);
+const lineMat = new THREE.LineBasicMaterial({ color: 0xff0000 });
+const tubeLines = new THREE.LineSegments(edges, lineMat);
+scene.add(tubeLines);
+
+const numBoxes = 55;
+const size = 0.075;
+const boxGeo = new THREE.BoxGeometry(size, size, size);
+for (let i = 0; i < numBoxes; i += 1) {
+    const boxMat = new THREE.MeshBasicMaterial({
+        color: 0xffffff,
+        wireframe: true
+    });
+    const box = new THREE.Mesh(boxGeo, boxMat);
+    const p = (i / numBoxes + Math.random() * 0.1) % 1;
+    const pos = tubeGeo.parameters.path.getPointAt(p);
+    pos.x += Math.random() - 0.4;
+    pos.z += Math.random() - 0.4;
+    box.position.copy(pos);
+    const rote = new THREE.Vector3(
+        Math.random() * Math.PI,
+        Math.random() * Math.PI,
+        Math.random() * Math.PI
+    );
+    box.rotation.set(rote.x, rote.y, rote.z);
+    const edges = new THREE.EdgesGeometry(boxGeo, 0.2);
+    const color = new THREE.Color().setHSL(p, 1, 0.5);
+    const lineMat = new THREE.LineBasicMaterial({ color: color });
+    const boxLines = new THREE.LineSegments(edges, lineMat);
+    boxLines.position.copy(pos);
+    boxLines.rotation.set(rote.x, rote.y, rote.z);
+    // scene.add(box);
+    scene.add(boxLines);
+}
+
+function updateCamera(t) {
+    const time = t * 0.1;
+    const looptime = 8 * 1000;
+    const p = (time % looptime) / looptime;
+    const pos = tubeGeo.parameters.path.getPointAt(p);
+    const lookAt = tubeGeo.parameters.path.getPointAt((p + 0.03) % 1);
+    camera.position.copy(pos);
+    camera.lookAt(lookAt);
+}
+
+function animate(t = 0) {
     requestAnimationFrame(animate);
-
-	renderer.render(scene, camera);
+    updateCamera(t);
+	// renderer.render(scene, camera);
+    composer.render(scene, camera);
     controls.update();
 }
 
