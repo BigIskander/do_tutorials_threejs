34c83d72374e609e6544fd5fb5170c045f229f5b
BigIskander <bigiskander@gmail.com>
2024-06-26 13:36:55 +0300

    Add texture.

--- earth_3/index.js
+++ earth_3/index.js
@@ -1,28 +1,31 @@
 import * as THREE from "three";
+import { OrbitControls } from "jsm/controls/OrbitControls.js";
 
 const w = window.innerWidth;
 const h = window.innerHeight;
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
 camera.position.z = 5;
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(w, h);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry();
+new OrbitControls(camera, renderer.domElement);
+const loader = new THREE.TextureLoader();
+const geometry = new THREE.IcosahedronGeometry(1, 12);
 const material = new THREE.MeshStandardMaterial({ 
-    color: 0xffff00 
+    map: new THREE.TextureLoader().load("./textures/00_earthmap1k.jpg")
 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const earthMesh = new THREE.Mesh(geometry, material);
+scene.add(earthMesh);
 
-const hemiLight = new THREE.HemisphereLight(0x0099ff, 0xaa5500);
+const hemiLight = new THREE.HemisphereLight(0xffffff, 0x000000);
 scene.add(hemiLight);
 function animate() {
     requestAnimationFrame(animate);
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.02;
+	earthMesh.rotation.x += 0.001;
+	earthMesh.rotation.y += 0.002;
 	renderer.render( scene, camera );
 }
 
