c3d610ce59e47249a9d8cb277557c4a258a0c503
BigIskander <bigiskander@gmail.com>
2024-06-26 16:08:52 +0300

    A tube.

--- flytru_wireframe/index.js
+++ flytru_wireframe/index.js
@@ -15,12 +15,22 @@ const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 controls.dampingFactor = 0.03;
 
-console.log(spline);
+// create a line geometry from the spline
+const points = spline.getPoints(100);
+const geometry = new THREE.BufferGeometry().setFromPoints(points);
+const material = new THREE.LineBasicMaterial({ color: 0xff0000 });
+const line = new THREE.Line(geometry, material);
+// scene.add(line);
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshStandardMaterial({ 
-    color: 0xffff00 
+// create a tube geometry from the spline
+const tubeGeo = new THREE.TubeGeometry(spline, 222, 0.65, 16, true);
+const tubeMat = new THREE.MeshStandardMaterial({
+    color: 0x0099ff,
+    side: THREE.DoubleSide,
+    wireframe: true,
 });
+const tube = new THREE.Mesh(tubeGeo, tubeMat);
+scene.add(tube);
 
 const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444);
 scene.add(hemiLight);
