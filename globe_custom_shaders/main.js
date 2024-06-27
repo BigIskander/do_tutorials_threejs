4d3b3147c40a08582d79f5e62ec9eb31102884ca
BigIskander <bigiskander@gmail.com>
2024-06-27 15:47:04 +0300

    Globe the beginning.

---
+++ globe_custom_shaders/main.js
@@ -0,0 +1,35 @@
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+    75,
+    innerWidth / innerHeight,
+    0.1,
+    1000
+);
+
+const renderer = new THREE.WebGLRenderer({
+    antialias: true
+});
+renderer.setSize(innerWidth, innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+document.body.appendChild(renderer.domElement);
+
+// create a sphere
+const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(5, 50, 50), 
+    new THREE.MeshBasicMaterial({
+        // color: 0xFF0000
+        map: new THREE.TextureLoader().load("./01-3.jpg")
+    })
+);
+
+scene.add(sphere);
+
+camera.position.z = 15;
+
+function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+}
+animate();
