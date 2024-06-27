119d63c40763601db768f09c8519aa9371ebc3c0
BigIskander <bigiskander@gmail.com>
2024-06-27 20:11:01 +0300

    Add stars.

--- globe_custom_shaders/main.js
+++ globe_custom_shaders/main.js
@@ -71,6 +71,19 @@ const starMaterial = new THREE.PointsMaterial({
     color: 0xffffff
 });
 
+const starVertices = []
+for (let i = 0; i < 10000; i++) {
+    const x = (Math.random() - 0.5) * 2000;
+    const y = (Math.random() - 0.5) * 2000;
+    const z = -Math.random() * 2000;
+    starVertices.push(x, y, z);
+}
+
+starGeometry.setAttribute('position', new THREE.Float32BufferAttribute(starVertices, 3));
+
+const stars = new THREE.Points(starGeometry, starMaterial);
+scene.add(stars);
+
 camera.position.z = 15;
 
 const mouse = {
