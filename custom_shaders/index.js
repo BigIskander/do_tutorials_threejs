bae525ea4de507c88511f27be12ab2a0a863ed65
BigIskander <bigiskander@gmail.com>
2024-06-29 14:05:34 +0300

    Some boiler plate code. Empty scene.

---
+++ custom_shaders/index.js
@@ -0,0 +1,30 @@
+import * as THREE from "three";
+import { OrbitControls } from "jsm/controls/OrbitControls.js";
+
+const w = window.innerWidth;
+const h = window.innerHeight;
+const scene = new THREE.Scene();
+scene.fog = new THREE.FogExp2(0x000000, 0.3);
+const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
+camera.position.z = 5;
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(w, h);
+document.body.appendChild(renderer.domElement);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.03;
+
+function update() {
+    renderer.render(scene, camera);
+    controls.update();
+}
+
+renderer.setAnimationLoop(update);
+
+function handleWindowResize () {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+window.addEventListener('resize', handleWindowResize, false);
