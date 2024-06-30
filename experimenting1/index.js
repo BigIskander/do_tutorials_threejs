93f7d27985cc2cdfba278020208185b685f498ea
BigIskander <bigiskander@gmail.com>
2024-06-30 18:30:00 +0300

    Boilerplate code.

---
+++ experimenting1/index.js
@@ -0,0 +1,29 @@
+import * as THREE from "three";
+import { OrbitControls } from "jsm/controls/OrbitControls.js";
+
+const w = window.innerWidth;
+const h = window.innerHeight;
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
+camera.position.z = 2;
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(w, h);
+document.body.appendChild(renderer.domElement);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.03;
+
+function update(timestamp, timeDiff) {
+    controls.update();
+    renderer.render(scene, camera);
+}
+
+renderer.setAnimationLoop(update);
+
+function handleWindowResize () {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+window.addEventListener('resize', handleWindowResize, false);
